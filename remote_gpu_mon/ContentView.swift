9cd8087dfb0f5ea6ddf7becc82d5bcf128d5a795
shg8 <me@shg8.com>
2026-03-02 14:11:45 -0800

    Initial Commit

---
+++ remote_gpu_mon/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  remote_gpu_mon
+//
+//  Created by Steven on 3/2/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ remote_gpu_mon/remote_gpu_monApp.swift
@@ -0,0 +1,17 @@
+//
+//  remote_gpu_monApp.swift
+//  remote_gpu_mon
+//
+//  Created by Steven on 3/2/26.
+//
+
+import SwiftUI
+
+@main
+struct remote_gpu_monApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
